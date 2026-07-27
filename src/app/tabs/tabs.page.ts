faef8452b17ca8c7bde62d37462e4cb1c05d6e42
paulinaMunoz01 <paulina.munoz.martinez3@gmail.com>
2026-07-26 20:01:42 -0600

    merge de ramas y cambio en tabs

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,7 +1,8 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { triangle, ellipse, square } from 'ionicons/icons';
+// Importamos los iconos en formato camelCase
+import { homeOutline, pricetagsOutline, addCircleOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
@@ -13,6 +14,11 @@ export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({ triangle, ellipse, square });
+    // Registramos los iconos para poder usarlos en el HTML
+    addIcons({ 
+      homeOutline, 
+      pricetagsOutline, 
+      addCircleOutline 
+    });
   }
 }
